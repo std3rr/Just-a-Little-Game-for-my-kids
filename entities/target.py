b23a9743a81cc7298827a1b1947a81596a2d9f59
Andreas Lewitzki <40644185+std3rr@users.noreply.github.com>
2018-07-30 16:58:07 +0200

    Add files via upload
    added levels and more dynamic targets

--- entities/target.py
+++ entities/target.py
@@ -1,6 +1,8 @@
 import pyglet
 from system.component import Component
 import config
+import random
+import time as t
 
 class Target(Component):
 
@@ -12,6 +14,7 @@ class Target(Component):
         self.speed = kwargs.get('speed', 5)
         self.name = kwargs.get('name', 'andreas')
         self.state = kwargs.get('state', 'moving')
+        self.z = kwargs.get('z', 2.5)
         pyglet.media.load('assets\\pappa1.wav', streaming=False).play()
         self.images = {
             'andreas' : [
@@ -38,9 +41,20 @@ class Target(Component):
 
         self.x_direction = 1
         self.y_direction = 1
-        self.z = 2.5
         self.hold = 64
+        self.opacity = 255
 
+        self.sprite.update(scale = self.z)
+        self.mul_z = 2.5 # placeholder / mutator for z
+
+        self.prob_change_x = random.random()
+        self.prob_change_y = random.random()
+        self.last_change_x_time = int(t.time())
+        self.last_change_y_time = int(t.time())
+        self.id = 0
+
+    def set_id(self,idx):
+        self.id = idx
 
 
     def check_hit(self, x, y, z):
@@ -50,8 +64,8 @@ class Target(Component):
         self.max_y = self.y + self.anchor_y
         if x > self.min_x and y > self.min_y and \
            x < self.max_x and y < self.max_y:
-           if z < 1 and z > 0.2:
-               pyglet.media.load('assets\\CAN.WAV', streaming=False).play()
+           if z < 1 * self.z and z > 0.2 * self.z:
+               pyglet.media.load('assets\\hit.wav', streaming=False).play()
                self.sprite = pyglet.sprite.Sprite(self.images[self.name][1], self.x, self.y)
                self.state = 'shot'
                return True
@@ -67,14 +81,14 @@ class Target(Component):
         # Two states, either target is shot or its moving
         if self.state == 'shot':
             self.hold -= 1
-            self.z -= 0.008
+            self.z -= 0.03
             self.y -= 5
             if self.z < 0:
                 self.z = 0
-            opacity = int(255 * (self.z/2.5))
+            self.opacity -= 4 if self.opacity > 0 else 0
             self.sprite.set_position(self.x, self.y)
-            self.sprite.update(scale = self.z-1.8, rotation = int(360 * self.z * 2))
-            self.sprite.opacity = opacity
+            self.sprite.update(scale = self.z, rotation = int((self.y % 356)))
+            self.sprite.opacity = self.opacity
             if self.hold <= 0:
                 self.sprite.delete()
         elif self.state == 'moving':
@@ -83,11 +97,36 @@ class Target(Component):
 
             self.sprite.set_position(self.x, self.y)
 
-            if self.x < self.anchor_x or (self.x + self.anchor_x) > (config.window_width):
+
+            if int(t.time()) - self.last_change_x_time > 1:
+                if random.random() <= self.prob_change_x:
+                    self.x_direction *= -1
+                self.last_change_x_time = int(t.time())
+
+            if int(t.time()) - self.last_change_y_time > 1:
+                if random.random() <= self.prob_change_y:
+                    self.y_direction *= -1
+                self.last_change_y_time = int(t.time())
+
+            if self.x < self.anchor_x:
+                print(f'set to {self.anchor_x}')
+                self.x = self.anchor_x
+                self.x_direction *= -1
+            if (self.x + self.anchor_x) > (config.window_width):
+                self.x = (config.window_width-self.anchor_x)
                 self.x_direction *= -1
 
-            if self.y < self.anchor_y or (self.y + self.anchor_y) > (config.window_height):
+            if self.y < self.anchor_y:
+                self.y = self.anchor_y
                 self.y_direction *= -1
+            if (self.y + self.anchor_y) > (config.window_height):
+                self.y = (config.window_height-self.anchor_y)
+                self.y_direction *= -1
+
+            if self.x_direction == 1:
+                self.sprite.update(rotation = 2*self.speed)
+            elif self.x_direction == -1:
+                self.sprite.update(rotation = -(2*self.speed))
 
 
 
